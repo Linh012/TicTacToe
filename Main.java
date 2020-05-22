8d17ff6daadffcbfcbb6a9458217693e65e4fdc2
Linh012 <gln8984@gmail.com>
2020-05-22 17:36:11 +0200

    Add comments

--- Main.java
+++ Main.java
@@ -17,7 +17,7 @@ class Main {
             col = input.nextInt();
             b.makeMove(row, col, player);
 
-            if (player == 'X') {
+            if (player == 'X') { //change player turn
                 player = 'O';
             } else {
                 player = 'X';
@@ -27,11 +27,11 @@ class Main {
         }
     }
 
-    public static class Board {
+    public static class Board { //Board object
         private char[][] board = new char[3][3];
         private int moves = 0;
 
-        public void makeMove(int row, int col, char player) {
+        public void makeMove(int row, int col, char player) { //make move
             if (board[row][col] == 'X' || board[row][col] == 'O') {
                 System.out.println("Spot already occupied!");
             } else {
@@ -40,11 +40,11 @@ class Main {
             }
         }
 
-        public boolean isFull() {
+        public boolean isFull() { //no move left/available
             return moves == 9;
         }
 
-        public boolean checkWinner() {
+        public boolean checkWinner() { //check board state for winner
             return (board[0][0] == board[0][1] && board[0][0] == board[0][2]) ||
                 (board[0][0] == board[1][1] && board[0][0] == board[2][2]) ||
                 (board[0][0] == board[1][0] && board[0][0] == board[2][0]) ||
@@ -55,7 +55,7 @@ class Main {
                 (board[1][0] == board[1][1] && board[1][0] == board[1][2]);
         }
 
-        public void display() {
+        public void display() { //print board
             for (int i = 0; i < board.length; i++) {
                 for (int j = 0; j < board[i].length; j++) {
                     System.out.print(board[i][j] + " ");
